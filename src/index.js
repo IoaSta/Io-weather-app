cac87f52b54a40f3741af38c3810189ce6e33d32
IoaSta <istavrati@gmail.com>
2024-12-11 17:53:33 +0100

    Small css changes, integrated forecast api in js

--- src/index.js
+++ src/index.js
@@ -17,6 +17,7 @@ function refreshWeather(response) {
   temperatureElement.innerHTML = Math.round(temperature);
   iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon"
     />`;
+  getForecast(response.data.city);
 }
 function formatDate(date) {
   let minutes = date.getMinutes();
@@ -49,25 +50,42 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
-function displayForecast() {
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[date.getDay()];
+}
+
+function getForecast(city) {
+  let apiKey = "b319bb0553to0644aae8dd09f2b4aeba";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  axios(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
-  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
   let forecastHtml = "";
 
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
 <div class="weather-forecast-day">
-<div class="weather-forecast-date">${day}</div>
- <div class="weather-forecast-icon">⛅</div> 
+<div class="weather-forecast-date">${formatDay(day.time)}</div>
+ <div >
+ <img src="${day.condition.icon_url}"class="weather-forecast-icon"/>
+ </div> 
 <div class="weather-forecast-temperatures">
 <div class="weather-forecast-temperature">
-<strong>15℃</strong></div> 
-<div class="weather-forecast-temperature">9℃</div>
+<strong>${Math.round(day.temperature.maximum)}°</strong></div> 
+<div class="weather-forecast-temperature">${Math.round(
+          day.temperature.minimum
+        )}°</div>
 </div>
 </div>
 `;
+    }
   });
 
   forecastElement.innerHTML = forecastHtml;
@@ -76,4 +94,3 @@ let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Thessaloniki");
-displayForecast();
